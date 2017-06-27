a3cf6adc80e0aaeef58faefdd364a91ee42a90ba
Adrián <Adrin@Adrin-PC>
2017-06-26 23:07:47 -0500

    eliminacion de los namedquerys

--- EducaT-ejb/src/main/java/ec/edu/espe/EducaT/model/ProgramaAlumno.java
+++ EducaT-ejb/src/main/java/ec/edu/espe/EducaT/model/ProgramaAlumno.java
@@ -12,8 +12,6 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -24,8 +22,7 @@ import javax.validation.constraints.Size;
  */
 @Entity
 @Table(name = "programa_alumno", catalog = "educat", schema = "")
-@NamedQueries({
-    @NamedQuery(name = "ProgramaAlumno.findAll", query = "SELECT p FROM ProgramaAlumno p")})
+
 public class ProgramaAlumno implements Serializable {
 
     private static final long serialVersionUID = 1L;
