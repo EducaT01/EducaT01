b74ccf6d1c182b7634499c8d93ed3d97d638ec77
David Suarez <dasurezm@espe.edu.ec>
2017-06-28 19:57:24 -0500

    Se agrego las clases CapacitacionFacade,CursoFacade y DocenteFacade en el paquete dao.

---
+++ EducaT-ejb/src/main/java/ec/edu/espe/EducaT/dao/CapacitacionFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.EducaT.dao;
+
+import ec.edu.espe.EducaT.model.Capacitacion;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author dovac
+ */
+@Stateless
+public class CapacitacionFacade extends AbstractFacade<Capacitacion> {
+
+    @PersistenceContext(unitName = "EducaT_EducaT-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CapacitacionFacade() {
+        super(Capacitacion.class);
+    }
+    
+}
